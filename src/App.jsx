00d35e0557bf328b802b28a70044759b2c7378f2
Ruben Meza <rmezar@gmail.com>
2021-07-26 23:35:10 -0500

    react basics & working with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-function App() {
+import Expenses from "./components/Expenses"
+
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Toilet Paper',
+      amount: 94.12, 
+      date: new Date(2021, 7, 1)
+    },
+    {
+      id: 2,
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 4, 30)
+    },
+    {
+      id: 3,
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 6, 14)
+    },
+    {
+      id: 4,
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12)
+    },
+  ]
+
   return (
-		<div>
+    <div>
       <h2>Let 's get started!</h2>
-		</div>
+      <Expenses items={expenses} />
+    </div>
   )
 }
 
