2b701a95b5258590957bcc69bae6def4594938be
Ruben Meza <rmezar@gmail.com>
2021-07-30 09:17:20 -0500

    fix month label

--- src/components/ExpensesChart.jsx
+++ src/components/ExpensesChart.jsx
@@ -8,7 +8,7 @@ const ExpensesChart = (props) => {
   const chartDataPoints = [
     { label: 'Jan', value: 0 },
     { label: 'Feb', value: 0 },
-    { label: 'May', value: 0 },
+    { label: 'Mar', value: 0 },
     { label: 'Apr', value: 0 },
     { label: 'May', value: 0 },
     { label: 'Jun', value: 0 },
